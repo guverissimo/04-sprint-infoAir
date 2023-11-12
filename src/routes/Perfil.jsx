e3e023ca589d12d5cfbe5a834d16a5796d63fb41
Gustavo Verissimo <gustavo.verissimo1@outlook.com>
2023-11-12 03:52:34 -0300

    add perfil.jsx

--- src/routes/Perfil.jsx
+++ src/routes/Perfil.jsx
@@ -1,10 +1,29 @@
-import {} from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Perfil = () => {
 
+    const {id} = useParams()
+
+    const [dados, setDados] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/usuarios/${id}`)
+        .then((res) => {return res.json() })
+        .then((data) => {
+            setDados(data)
+        })
+    }, [])
+        
+
     return (
         <> 
-        
+            <div className="perfil">
+                {dados.nome}
+                {dados.email}
+                {dados.tel}
+                {dados.endereco}
+            </div>
         </>
     )
 }
